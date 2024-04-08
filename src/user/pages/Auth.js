69a11ce65be3207b35fcd95e9a704970db8a2088
Henry <henrywl@gmail.com>
2024-04-09 00:46:38 +0100

    app wide context

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Input from "../../shared/components/FormElements/Input";
 import { useForm } from "../../shared/hooks/form-hook";
 import {
@@ -10,9 +10,12 @@ import Button from "../../shared/components/FormElements/Button";
 
 import "./Auth.css";
 import Card from "../../shared/components/UIElements/Card";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
@@ -55,6 +58,7 @@ const Auth = () => {
     // send to backend!
     event.preventDefault();
     console.log(formState.inputs);
+    auth.login();
   };
 
   return (
